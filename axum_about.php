69bac0bfa32107dc63b913b43db9b916aa203cf0
axumg <agebregorgis@mail.greenriver.edu>
2017-02-14 13:30:03 -0800

    older about.php not used

---
+++ axum_about.php
@@ -0,0 +1,60 @@
+<?php
+/**
+The MIT License (MIT)
+
+Copyright (c) 2017 Stacey Demecilio, Shimbey Assie, Axumawit Gebregorgis
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ */
+
+ include ('menu.php');
+?>
+<!DOCTYPE>
+    <html>
+    <head>
+        <title>GR Repair Shop</title>
+        <!-- CSS libraries -->
+        <link rel = "stylesheet" href = "css/RepairShop.css">
+        <link rel = "stylesheet" href = "//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    </head>
+    
+    <body>
+        <h2><u>Our Story</u></h2>
+        <p>The Green River PC Repair shop opened January 6, 2011 by Spunky. The PC Repair shop is completely student ran but is under faculty supervision. The students that work in the shop use the time they are there for some hands on, real world experience while also still getting an education. Some of the students may also volunteer their time to the shop.</p>
+        
+        <h2><u>Shop Requirements</u></h2>
+        <p><b>Cost:</b><br>
+        We do not charge per hour for services. However, there is a <b><u>suggested donation</u></b> of <b><u>$50.00</u></b> that goes towards diagnosis/repair of your equipment and the cost to run the shop.</p>
+        
+        <p><b>Repair:</b><br>
+        We will with in our power, diagnosis a problem and make suggestions and recommendations as needed if applicable.</p>
+        
+        <p><b>Software Upgrades:</b><br>
+        While your equipment is with us and authorized we can update certain software.</p>
+        
+        <p><b>Hardware/Software:</b><br>
+        We can install hardware and/or software. It does have to be purchased and brought into the shop, we do not order parts or software.</p>
+        
+        <p><b>Support Devices:</b><br>
+        We will work on almost all laptops as long as they are in English. No tablets, cell phones, flash drives, or ay Apple product. </p>
+        
+        <p><b>Find and Delete Viruses:</b><br>
+        We will scan a device and if we do discover something, we will remove if possible, or provide options.</p>
+        
+        <p><b>Upgrade Your Hardware:</b><br>
+        If you have compatible hardware, we can install it for you, or make suggestions if requested.</p>
+        
+        <p><b>For Any Work Done (including general questions):</b><br>
+        We do have paperwork that has to be completed before we can start diagnosis of equipment.<p>
+        
+        <!-- including the jquery library from the jquery website -->
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script src = "javascript/RepairShop.js"></script>
+    </body>
+    </html>
