89ada0c769a727b990316cc2737bd72d6e1c5be3
sdemecilio <stacey.demecilio@live.com>
2017-02-21 01:31:32 -0800

    fixed page so radio buttons on form appear

--- forms.php
+++ forms.php
@@ -5,38 +5,6 @@
  * Date: 2/17/2017
  * Time: 11:09 AM
  */
-
-
-
-
-/**
-
-The MIT License (MIT)
-
-
-
-Copyright (c) 2017 Stacey Demecilio, Shimbey Assie, Axumawit Gebregorgis
-
-
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-
-
- */
-
-
-
-
-
 ?>
 
 
@@ -59,10 +27,13 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
     <title>Green River Repair Shop</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
     <link rel="stylesheet" href="assets/css/main.css" />
     <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
     <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+    
 </head>
 <body>
 
@@ -90,13 +61,25 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
                     <header class="major">
                         <h2>Forms</h2>
                     </header>
-                        <form action="">
+                        <form>
                             <p>Is your computer under warranty?</p>
-                            <input type="radio" name="warranty" value="yes" id = "YesWarranty"> Yes<br>
-                            <input type="radio" name="warranty" value="no" id = "no"> No<br>
+                            <input type = "radio" name = "warranty" id = "YesWarranty"><label for = "YesWarranty">Yes</label>
+                            <input type = "radio" name = "warranty" id = "no"><label for = "no">No</label>
+                            
+                            <p id = "followUp">Is your warranty a software warranty or hardware warranty?<br><br>
+                            
+                                <input type = "radio" name = "followUp" id = "Software"><label for = "Software">Software</label>
+                                <input type = "radio" name = "followUp" id = "Hardware"><label for = "Hardware">Hardware</label>
+                                <input type = "radio" name = "followUp" id = "Unsure"><label for = "Unsure">Not sure/ Don't know</label>
+                            </p>
+                            
+<!--  <p id = "options">
+    <input type = "radio" name = "followUpAnswer" value = "Software" id = "Software"> Software<br>
+    <input type = "radio" name = "followUpAnswer" value = "Hardware" id = "Hardware"> Hardware
+  </p>-->
                         </form>
                         <p id = "errorMessage" class = "requiredFields">We cannot work on your computer. Please see a tech for questions.</p>
-                        <button> <a href = "GRworkOrderForm.php">Green River Community</a></button>&nbsp;&nbsp;<button>Public</button>
+                        <a class = "btn btn-default" href = "policy.php" id = "proceedToPaperwork">Proceed to paperwork</a>
                 </div>
 
             </div>
@@ -112,19 +95,16 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 </div>
 
-<!-- including the jquery library from the jquery website -->
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<script src = "javascript/RepairShop.js"></script>
-
 <!-- Scripts -->
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/jquery.scrollex.min.js"></script>
-<script src="assets/js/jquery.scrolly.min.js"></script>
+
 <script src="assets/js/skel.min.js"></script>
 <script src="assets/js/util.js"></script>
 <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
 <script src="assets/js/main.js"></script>
+<!-- including the jquery library from the jquery website -->
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src = "javascript/RepairShop.js"></script>
 
 </body>
 </html>
