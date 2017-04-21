c779cb70ac66a8ceacade456e563eabae08b6ed1
ShimbeyA <shimbeyassie@yahoo.com>
2017-04-20 23:09:52 -0700

    nav bar menu

--- admin/mainMenu.php
+++ admin/mainMenu.php
@@ -19,19 +19,43 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 <!DOCTYPE html>
 <html>
 
-<body>
+<head>
+ <link rel = "stylesheet" href = "adminMenu.css">
+ <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 
-<!--creating admin nav bar-->
-<nav id="nav">
-    <ul>
-        <li><a href="../index.php">Our Story</a></li>
-        <li><a href = "../shopRequirements.php">Shop Requirements</a></li>
+</head>
+ 
+<body>
+<!--Creating nav bar -->
+
+
+<nav>
+     <ul>
+     	
+
+     	<li><a href="../index.php">Our Story</a></li>
         <li><a href="../forms.php">Forms</a></li>
+        <li><a href = "../shopRequirements.php">Shop Requirements</a></li>        
         <li><a href="../contact.php">Contact</a></li>
         <li><a href="login.php">Admin/Tech Login</a></li>
-    </ul>
+   </ul>
+   <div class="handle">Menu</div>
 </nav>
 
+<script>
+	$(".handle").on("click", function(){
+	
+    	    $("nav ul").toggleClass("showing");
+   });
+
+</script>
+
 </body>
-</html>
+</html> 
+
+<!DOCTYPE html>
+<html>
+
+<body>
+
 
